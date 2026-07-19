2c4b49bb5b7323a47930c8bd10d1a5dfc188ceeb
Pablito1914/restaurants <biomagnetismomedicogb@gmail.com>
2026-07-19 06:18:10 -0500

    Fix: Ajustar version de httpx para compatibilidad con Supabase

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from pydantic import BaseModel
 import uuid
 
 # 1. Configuración de Supabase
-SUPABASE_URL = "https://lnjvqatjbjpcaqumrpwp.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuanZxYXRqYmpwY2FxdW1ycHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNTMzMzcsImV4cCI6MjA5OTkyOTMzN30.KVrbYMHl5p9vgaus1YIFkgwQjMe-lB-sKAYjt6Ycqyw"
+SUPABASE_URL = os.environ.get("https://lnjvqatjbjpcaqumrpwp.supabase.co")
+SUPABASE_KEY = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuanZxYXRqYmpwY2FxdW1ycHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNTMzMzcsImV4cCI6MjA5OTkyOTMzN30.KVrbYMHl5p9vgaus1YIFkgwQjMe-lB-sKAYjt6Ycqyw")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # 2. Inicialización de la aplicación FastAPI
